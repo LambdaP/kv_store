9a94bba2ea619299eca6cb8c70eac27d14b1a773
Patrick Lambein-Monette <patrick@lambein.name>
2024-09-06 23:30:12 +0200

    basic key-value data structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+
+struct KvStore {
+    store: HashMap<String, String>,
+}
+
+impl KvStore {
+    fn insert(&mut self, key: String, value: String) -> Option<String> {
+        self.store.insert(key, value)
+    }
+
+    fn get(&self, key: &str) -> Option<String> {
+        self.store.get(key).cloned()
+    }
+
+    fn remove(&mut self, key: &str) -> Option<String> {
+        self.store.remove(key)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
