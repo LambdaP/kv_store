84c7814e5378c8a5b3ed532cdf40475e3676254c
Patrick Lambein-Monette <patrick@lambein.name>
2024-09-10 18:33:56 +0200

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,41 @@ use axum::{
     Router,
 };
 
+use tracing::{debug, info};
+
 #[derive(Debug, Default, Clone)]
 struct KvStore(Arc<RwLock<HashMap<String, String>>>);
 
 impl KvStore {
+    #[tracing::instrument(level = "trace", skip())]
     fn new() -> KvStore {
-        Default::default()
+        KvStore::default()
     }
 
+    #[tracing::instrument(level = "trace", skip(self, key, value))]
     async fn insert(&mut self, key: String, value: String) -> Option<String> {
+        debug!("Inserting key: {}", key);
         self.0.write().await.insert(key, value)
     }
 
+    #[tracing::instrument(level = "trace", skip(self, key))]
     async fn get(&self, key: &str) -> Option<String> {
+        debug!("Getting key: {}", key);
         self.0.read().await.get(key).cloned()
     }
 
+    #[tracing::instrument(level = "trace", skip(self, key))]
     async fn remove(&mut self, key: &str) -> Option<String> {
+        debug!("Removing key: {}", key);
         self.0.write().await.remove(key)
     }
 }
 
-
+#[tracing::instrument(level = "trace", skip())]
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     let kv_store = KvStore::new();
 
     let app = Router::new()
@@ -47,21 +58,39 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
+#[tracing::instrument(level = "trace", skip(kv_store))]
 async fn get_key(State(kv_store): State<KvStore>, Path(key): Path<String>) -> impl IntoResponse {
-    kv_store.get(&key).await.ok_or(StatusCode::NOT_FOUND)
+    if let Some(value) = kv_store.get(&key).await {
+        info!("Key found: {}", key);
+        debug!("Value: {}", value);
+        Ok(value)
+    } else {
+        info!("Key not found: {}", key);
+        Err(StatusCode::NOT_FOUND)
+    }
 }
 
-async fn put_key_val(State(mut kv_store): State<KvStore>, Path(key): Path<String>, value: String) {
+#[tracing::instrument(level = "trace", skip(kv_store, value))]
+async fn put_key_val(
+    State(mut kv_store): State<KvStore>,
+    Path(key): Path<String>,
+    value: String,
+) -> impl IntoResponse {
     kv_store.insert(key, value).await;
+    info!("Key inserted.");
+    StatusCode::OK
 }
 
+#[tracing::instrument(level = "trace", skip(kv_store))]
 async fn delete_key(
     State(mut kv_store): State<KvStore>,
     Path(key): Path<String>,
 ) -> impl IntoResponse {
-    kv_store
-        .remove(&key)
-        .await
-        .map(|_| ())
-        .ok_or(StatusCode::NOT_FOUND)
+    if kv_store.remove(&key).await.is_some() {
+        info!("Key deleted: {}", key);
+        Ok(())
+    } else {
+        info!("Key not found for deletion: {}", key);
+        Err(StatusCode::NOT_FOUND)
+    }
 }
