b207918bac2482ac23f2736716d2c8e245a2da43
Patrick Lambein-Monette <patrick@lambein.name>
2024-09-10 11:54:42 +0200

    Skeleton HTTP key-value store using Axum

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,67 @@
 use std::collections::HashMap;
+use std::sync::Arc;
 
-struct KvStore {
-    store: HashMap<String, String>,
-}
+use tokio::sync::RwLock;
+
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{delete, get, put},
+    Router,
+};
+
+#[derive(Debug, Default, Clone)]
+struct KvStore(Arc<RwLock<HashMap<String, String>>>);
 
 impl KvStore {
-    fn insert(&mut self, key: String, value: String) -> Option<String> {
-        self.store.insert(key, value)
+    fn new() -> KvStore {
+        Default::default()
     }
 
-    fn get(&self, key: &str) -> Option<String> {
-        self.store.get(key).cloned()
+    async fn insert(&mut self, key: String, value: String) -> Option<String> {
+        self.0.write().await.insert(key, value)
     }
 
-    fn remove(&mut self, key: &str) -> Option<String> {
-        self.store.remove(key)
+    async fn get(&self, key: &str) -> Option<String> {
+        self.0.read().await.get(key).cloned()
+    }
+
+    async fn remove(&mut self, key: &str) -> Option<String> {
+        self.0.write().await.remove(key)
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    let kv_store = KvStore::new();
+
+    let app = Router::new()
+        .route("/store/:key", get(get_key))
+        .route("/store/:key", put(put_key_val))
+        .route("/store/:key", delete(delete_key))
+        .with_state(kv_store);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_key(State(kv_store): State<KvStore>, Path(key): Path<String>) -> impl IntoResponse {
+    kv_store.get(&key).await.ok_or(StatusCode::NOT_FOUND)
+}
+
+async fn put_key_val(State(mut kv_store): State<KvStore>, Path(key): Path<String>, value: String) {
+    kv_store.insert(key, value).await;
+}
+
+async fn delete_key(
+    State(mut kv_store): State<KvStore>,
+    Path(key): Path<String>,
+) -> impl IntoResponse {
+    kv_store
+        .remove(&key)
+        .await
+        .map(|_| ())
+        .ok_or(StatusCode::NOT_FOUND)
 }
